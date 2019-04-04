cf8300a168b4cf12f86c2af5d063b37bc57d0263
megan schulte and zsuzsanna mangu <zsuzsanna@>
2019-04-04 10:44:23 -0700

    add initial files

---
+++ js/scripts.js
@@ -0,0 +1,15 @@
+function romanNumbers(input) {
+  var romanNumerals = ["I", "V", "X", "L", "C", "D", "M"];
+  return false;
+};
+
+console.log(
+  romanNumbers("1") === "I"
+);
+
+
+  //
+  //
+  // var roman = roman.toUpperCase();
+  //
+  //  i = roman.length;
