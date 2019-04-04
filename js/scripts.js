cf4d95573a763c393c3121f578b3b8268066196a
megan schulte and zsuzsanna mangu <zsuzsanna@>
2019-04-04 14:02:15 -0700

    add for loops for roman symbols, 1000, 500,100,50,10,5,1

--- js/scripts.js
+++ js/scripts.js
@@ -1,17 +1,75 @@
+  // var ones = ["I", "V", "X"];
+  // var tens = ["X", "L", "C"];
+  // var tens = ["C", "D", "M"];
+  // var thousands = ["M"];
+var input = $("#input").val();
+
 function romanNumbers(input) {
 
-  var romanNumerals = ["I", "V", "X", "L", "C", "D", "M"];
-  return true;
+  var output = "";
 
-  if (typeof input !== "number") {
-    return false;
+  for (var i = input; i > 0; i-=1000) {
+    if (input >=1000) {
+      output += "M";
+      input -= 1000;
+    }
+
+  for (var i = input; i > 0; i-=500) {
+    if (input >=500) {
+      output += "D";
+      input -= 500;
+    }
+
+  for (var i = input; i > 0; i-=100) {
+    if (input >=100) {
+      output += "D";
+      input -= 100;
+    }
+
+  for (var i = input; i > 0; i-=50) {
+    if (input >=50) {
+      output += "D";
+      input -= 50;
+    }
+
+  for (var i = input; i > 0; i-=10) {
+    if (input >=10) {
+      output += "D";
+      input -= 10;
+    }
+
+    for (var i = input; i > 0; i-=5) {
+      if (input >=5) {
+        output += "D";
+        input -= 5;
+      }
+
+    for (var i = input; i > 0; i-=1) {
+      if (input >=1) {
+        output += "D";
+        input -= 1;
+      }
+
   }
+
+  return output;
+
 }
 
-
 console.log(
-  romanNumbers(1)
+ romanNumbers(2000),
+ romanNumbers(2000) === "MM"
 );
+
+
+
+
+
+
+
+
+
+
 
 
   //
@@ -19,3 +77,27 @@ console.log(
   // var roman = roman.toUpperCase();
   //
   //  i = roman.length;
+
+// function romanNumbers(input) {
+//
+//   if (typeof input !== "number") {
+//     return false;
+//   } else if (input === 1){
+//     return "I";
+//   } else if (input === 5){
+//     return "V";
+//   } else if (input === 10) {
+//     return "X";
+//   } else {
+//     console.log(error);
+//   }
+// }
+//
+// console.log(romanNumbers("h"))
+//
+
+   // } else if {
+   //   for(var i=1, i<=3, i++) {
+   //   return input += "I".join;
+   //   }
+   // }
